bd3c1e9b6274ed95ea0116b6b95228cf64d6ed3d
Burkey Koontz <bk.koontz@gmail.com>
2018-07-30 12:18:43 -0700

    added get endpoint that allows the client to request specific items

--- server.js
+++ server.js
@@ -14,6 +14,16 @@ const app = express();
 // ADD STATIC SERVER HERE
 app.use(express.static('public'));
 
+app.get('/api/notes', (req, res) => {
+  res.json(data);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+  const requestedItem = data.find(item => item.id === Number(id));
+  res.json(requestedItem);
+});
+
 app.listen(8080, function () {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
