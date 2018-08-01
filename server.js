d8d88d062de0b07ada70fca4a89e631778bb54eb
Burkey Koontz <bk.koontz@gmail.com>
2018-08-01 13:23:35 -0700

    added update (put) for notes

--- server.js
+++ server.js
@@ -16,11 +16,15 @@ console.log('Hello Noteful!');
 const express = require('express');
 const app = express();
 
+// server logs
+app.use(logger);
+
 // ADD STATIC SERVER HERE
 app.use(express.static('public'));
 
-// server logs
-app.use(logger);
+// parse incoming json requests to make them available on req.body
+app.use(express.json());
+
 
 
 app.get('/api/notes', (req, res, next) => {
@@ -40,6 +44,31 @@ app.get('/api/notes/:id', (req, res, next) => {
       return next(err);
     }
     res.json(item);
+  });
+});
+
+app.put('/api/notes/:id', (req, res, next) => {
+  const id = req.params.id;
+
+  /***** Never trust users - validate input *****/
+  const updateObj = {};
+  const updateFields = ['title', 'content'];
+
+  updateFields.forEach(field => {
+    if (field in req.body) {
+      updateObj[field] = req.body[field];
+    }
+  });
+
+  notes.update(id, updateObj, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.json(item);
+    } else {
+      next();
+    }
   });
 });
 
