417b756ca83afcd4668c10cb1970b93b151e07ce
Burkey Koontz <bk.koontz@gmail.com>
2018-08-01 07:15:01 -0700

    added config.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 // Load array of notes
 const data = require('./db/notes');
 
+const { PORT } = require('./config');
+
 console.log('Hello Noteful!');
 
 // INSERT EXPRESS APP CODE HERE...
@@ -13,6 +15,8 @@ const app = express();
 
 // ADD STATIC SERVER HERE
 app.use(express.static('public'));
+
+
 
 app.get('/api/notes', (req, res) => {
   if (req.query.searchTerm) {
@@ -28,7 +32,7 @@ app.get('/api/notes/:id', (req, res) => {
   res.json(requestedItem);
 });
 
-app.listen(8080, function () {
+app.listen(PORT, function () {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
   console.error(err);
